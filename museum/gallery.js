f9b10014589097c38386f2e2b52a562a36b9a0ea
Alexander Suslov <sasha_suslov07@mail.ru>
2021-09-27 16:03:56 +0300

    feat: add script for gallery

--- museum/gallery.js
+++ museum/gallery.js
@@ -1,7 +1,33 @@
-const pictureInnerContainer = document.querySelector('picture-inner-container');
+const pictureInnerContainer = document.querySelector('.picture-inner-container');
 
-const img = document.createElement('img');
-img.classList.add('gallery-img');
-img.src = `assets/galery1.jpg`;
-img.alt = `galery1`;
-pictureInnerContainer.append(img);
+
+const pictureMaxNum = 15;
+const maxHeight = 6090;
+const picture = [];
+
+
+for (let i = 0; i < pictureMaxNum; i++) {
+  picture[i] = document.createElement('img');
+  picture[i].classList.add('gallery-img'); 
+  picture[i].src = `./assets/gallery/galery${i + 1}.jpg`;
+  picture[i].alt = `galery${i + 1}`;
+}
+
+let newPicture = shuffle(picture);
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+
+for (let i = 0; i < pictureMaxNum; i++) {
+    pictureInnerContainer.append(newPicture[i]);
+    if (i == 10) {
+      newPicture[i].style.paddingTop = "50px";
+    }
+}
