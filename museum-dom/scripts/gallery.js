9076473f8cb5aa3cec43ca3d1a6a667e5e919b9f
Alexander Suslov <sasha_suslov07@mail.ru>
2021-10-09 12:20:59 +0300

    feat: rename working folder

---
+++ museum-dom/scripts/gallery.js
@@ -0,0 +1,33 @@
+const pictureInnerContainer = document.querySelector('.picture-inner-container');
+
+
+const pictureMaxNum = 15;
+const maxHeight = 6090;
+const picture = [];
+
+
+for (let i = 0; i < pictureMaxNum; i++) {
+  picture[i] = document.createElement('img');
+  picture[i].classList.add('gallery-img'); 
+  picture[i].src = `./assets/gallery/galery${i + 1}.jpg`;
+  picture[i].alt = `galery${i + 1}`;
+}
+
+let newPicture = shuffle(picture);
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+
+for (let i = 0; i < pictureMaxNum; i++) {
+    pictureInnerContainer.append(newPicture[i]);
+    if (i == 10 && screen.width > 768) {
+      newPicture[i].style.paddingTop = "50px";
+    }
+}
