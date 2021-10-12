9da9cc017a4827d63922afc2a646325ead7790a5
Alexander Suslov <sasha_suslov07@mail.ru>
2021-10-12 07:13:51 +0300

    feat: add slider to main video

---
+++ museum-dom/scripts/youtube.js
@@ -0,0 +1,15 @@
+// const youtubeVideos = document.querySelectorAll('.slde-screen');
+
+
+// youtubeVideos.forEach(vd => {
+//   vd.play.addEventListener('click', function () {
+//    vd.playVideo();
+//   });
+// });
+
+
+// youtubeVideos.forEach(vd => {
+//   vd.pause.addEventListener('click', function () {
+//    vd.pauseVideo();
+//   });
+// });
