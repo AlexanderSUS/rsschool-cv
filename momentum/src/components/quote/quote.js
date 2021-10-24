ebeff13eb24dd00d397d703d630456695cedc2bc
Alexander Suslov <sasha_suslov07@mail.ru>
2021-10-24 11:04:02 +0300

    feat: add quote application

--- momentum/src/components/quote/quote.js
+++ momentum/src/components/quote/quote.js
@@ -1,9 +1,17 @@
 export default getQuotes
 
+const quotesUrl = 'https://raw.githubusercontent.com/AlexanderSUS/rsschool-cv/momentum/momentum/src/components/quote/data.json';
+
+function pickRandom(arr) {
+  return Math.floor(Math.random() * arr.length);
+}
 
 async function getQuotes() {
-  const quotes = 'https://raw.githubusercontent.com/AlexanderSUS/rsschool-cv/momentum/src/components/quote/data.json';
-  const res = await fetch(quotes)
+  const res = await fetch(quotesUrl);
   const data =  await res.json();
-  console.log(data);
+  const index = pickRandom(data);
+  document.querySelector('.author').textContent = data[index].text;
+  document.querySelector('.quote').textContent = data[index].author;
 }
+
+document.querySelector('.change-quote').addEventListener('click', getQuotes);
